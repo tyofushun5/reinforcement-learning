a764440efc3a893b5db5833186e3698aa47fdaa0
shun <zenshun10@gmail.com>
2025-02-13 18:58:01 +0900

    強化学習　2/13 2 コミット

---
+++ RCJ_Reinforcement_learning/tools/rcjs_calculation_tools.py
@@ -0,0 +1,72 @@
+import math
+
+import pybullet as p
+
+class CalculationTools(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def is_ball_touch(attacker_id, ball_id, count):
+        is_touch = p.getContactPoints(ball_id, attacker_id)
+        if is_touch:
+            count = 0
+        return count
+
+    @staticmethod
+    def angle_calculation_id(a_id, b_id):
+        a_pos, _ = p.getBasePositionAndOrientation(a_id)
+        b_pos, _ = p.getBasePositionAndOrientation(b_id)
+        dx = b_pos[0] - a_pos[0]
+        dy = b_pos[1] - a_pos[1]
+        angle_radians = math.atan2(dx, dy)
+        angle_deg = math.degrees(angle_radians)
+        angle_deg = angle_deg % 360
+        angle_deg = round(angle_deg, 1)
+        return angle_deg
+
+    @staticmethod
+    def angle_calculation_pos(a_pos, b_pos):
+        dx = b_pos[0] - a_pos[0]
+        dy = b_pos[1] - a_pos[1]
+        angle_radians = math.atan2(dx, dy)
+        angle_deg = math.degrees(angle_radians)
+        angle_deg = angle_deg % 360
+        angle_deg = round(angle_deg, 1)
+        return angle_deg
+
+    @staticmethod
+    def euclidean_distance_id(a_id, b_id):
+        a_pos, _ = p.getBasePositionAndOrientation(a_id)
+        b_pos, _ = p.getBasePositionAndOrientation(b_id)
+        if len(a_pos) != len(b_pos):
+            raise ValueError
+        x1, y1 = a_pos[:2]
+        x2, y2 = b_pos[:2]
+        return math.hypot(x2 - x1, y2 - y1)
+
+    @staticmethod
+    def euclidean_distance_pos(a_pos, b_pos):
+        if len(a_pos) != len(b_pos):
+            raise ValueError
+        x1, y1 = a_pos[:2]
+        x2, y2 = b_pos[:2]
+        return math.hypot(x2 - x1, y2 - y1)
+
+    @staticmethod
+    def movement_reward_calculation(reward, pos, previous_pos, past_distance):
+        distance = CalculationTools.euclidean_distance_pos(pos, previous_pos)
+        if distance < past_distance:
+            reward += 0.5
+        else:
+            reward -= 0.5
+        return reward
+
+    @staticmethod
+    def distance_reward_calculation(reward, agent_pos, ball_pos, ball_past_distance):
+        distance = CalculationTools.euclidean_distance_pos(agent_pos, ball_pos)
+        if distance < ball_past_distance:
+            reward += 0.5
+        else:
+            reward -= 0.4
+        return reward
--- RCJ_Reinforcement_learning/tools/rcjs_reward_calculation.py
+++ RCJ_Reinforcement_learning/tools/rcjs_reward_calculation.py
@@ -12,4 +12,40 @@ class FirstRewardCalculation(RewardCalculation):
         super().__init__()
 
     def reward_calculation(self, hit_ids, attacker_id, ball_id, step_count):
-        pass
+        reward = 0
+        attacker_pos, _ = p.getBasePositionAndOrientation(attacker_id)
+
+        reward += self.movement_reward_calculation(reward,
+                                                   attacker_pos,
+                                                   self.previous_attacker_pos,
+                                                   self.past_distance)
+        self.previous_attacker_pos = attacker_pos
+
+        ball_pos, _ = p.getBasePositionAndOrientation(ball_id)
+
+        reward += self.distance_reward_calculation(reward,
+                                                   attacker_pos,
+                                                   ball_pos,
+                                                   self.ball_past_distance)
+
+        self.ball_past_distance = self.euclidean_distance_pos(attacker_pos, ball_pos)
+
+        is_touch = p.getContactPoints(ball_id, attacker_id)
+        if is_touch:
+            reward += 5
+        if hit_ids[self.my_goal_line_idx] == ball_id:
+            reward -= 10
+        if hit_ids[self.enemy_goal_line_idx] == ball_id:
+            reward += 10
+            self.is_goal = True
+        for i in range(len(hit_ids)):
+            if hit_ids[i] == attacker_id:
+                reward -= 0.2
+        angle = self.angle_calculation_id(attacker_id, ball_id)
+        if angle<=90 or angle>=270:
+            reward += 0.1
+        else:
+            reward -= 0.1
+        if angle<=45 or angle>=315:
+            reward += 0.2
+        return reward
