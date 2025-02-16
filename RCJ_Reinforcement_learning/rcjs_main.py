21cada6b27bf19855c50f72ca6334c6d438bab4e
shun <zenshun10@gmail.com>
2025-02-16 16:37:42 +0900

    強化学習　2/16  コミット

--- RCJ_Reinforcement_learning/rcjs_main.py
+++ RCJ_Reinforcement_learning/rcjs_main.py
@@ -7,6 +7,7 @@ from RCJ_Reinforcement_learning.environment.rcjs_environment import Environment
 save_dir = "model"
 os.makedirs(save_dir, exist_ok=True)
 
+
 def main():
     env = Environment()
 
@@ -15,13 +16,15 @@ def main():
                 device="cpu",
                 verbose=1,
                 n_steps=4096,
-                batch_size=256)
+                batch_size=256,
+                gamma=0.01)
 
     model.learn(total_timesteps=10000000)
 
     model.save(os.path.join(save_dir, "RCJ_ppo_model"))
 
     env.close()
+
 
 if __name__ == "__main__":
     main()
